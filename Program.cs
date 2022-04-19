8c2b5f8996d680047bfadb7376b36353027eaba7
AbdurrahmanRidho <97325407+AbdurrahmanRidho@users.noreply.github.com>
2022-04-19 12:42:58 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,13 +18,15 @@ namespace ResponsiPemograman4341
 
             Karyawan karyawan1 = new Karyawan(123, "Abdurrahman", 5000000);
             Karyawan karyawan2 = new Karyawan(456, "Ridho", 4000000);
+            Karyawan karyawan3 = new Karyawan(789, "Ahmad", -100000);
 
             Console.WriteLine("==================================================");
             Console.WriteLine(" NO | NIK / NAMA\t\t | Gaji Bulanan\t |");
             Console.WriteLine("==================================================");
             Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.Nama, karyawan1.GajiBulanan);
             Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.Nama, karyawan2.GajiBulanan);
-
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t\t |", karyawan3.NIK, karyawan3.Nama, karyawan3.GajiBulanan);
+            
             Console.WriteLine("\n\nAsikkkkk dpt kenaikan gaji nih!!!\n");
 
             Console.WriteLine("==================================================");
@@ -32,7 +34,7 @@ namespace ResponsiPemograman4341
             Console.WriteLine("==================================================");
             Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.Nama, karyawan1.NaikGaji);
             Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.Nama, karyawan2.NaikGaji);
-
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t\t |", karyawan3.NIK, karyawan3.Nama, karyawan3.GajiBulanan);
 
             Console.ReadKey();
         }
